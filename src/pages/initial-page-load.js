5c0575ddb800f305f1b1e0db8071fea02a1b11b3
thesudeshdas <sudeshkumardas7@gmail.com>
2020-12-12 21:15:15 +0530

    fix: pic

--- src/pages/initial-page-load.js
+++ src/pages/initial-page-load.js
@@ -9,7 +9,7 @@ const initialPageLoad = () => {
     
     homeTab.innerHTML = `
     <section>
-        <img class="hero-img" src="/dist/images/hero-food.jpg" alt="">
+        <img class="hero-img" src="./images/hero-food.jpg" alt="">
         <!-- https://unsplash.com/photos/-YHSwy6uqvk -->
         <h1 class="hero-heading">Eat with the Best</h1>
         <h3 class="hero-heading-description"> Welcome to The aethetic food stop. Eat the <span>Tastiest Delicacies</span> with a view to behold</h3>
